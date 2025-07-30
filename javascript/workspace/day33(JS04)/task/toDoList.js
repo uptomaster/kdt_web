b28e05305fc605d3c63ac9b3cbd24f1c827bfc0b
uptomaster <lnh929@naver.com>
2025-07-30 19:33:47 +0900

    250730 이남혁 day33 커밋

--- javascript/workspace/day33(JS04)/task/toDoList.js
+++ javascript/workspace/day33(JS04)/task/toDoList.js
@@ -1,9 +1,9 @@
 const buttonclick = document.getElementById("add-button")
 // input에서 값 가져오기
-
-buttonclick.addEventListener('click', ()=>{
+let count = 0;
+buttonclick.addEventListener('click', () => {
   let value = prompt("추가할 할일을 입력하세요:");
-
+  count++;
   const unorderlist = document.getElementById('task-list');
   const list = document.createElement("li"); // 리스트 요소를 만듦
 
@@ -12,19 +12,37 @@ buttonclick.addEventListener('click', ()=>{
   list.style.borderBottom = "0.5px solid black";
 
   list.style.padding = "3px";
-  
+
   let text3 = "<div>";
-  text3 += "<input type='checkbox'>";
+  text3 += "<input type='checkbox' class='check-button'>";
   text3 += "<a>";
   text3 += value;
   text3 += "</a>";
-  text3 += "<button>삭제</button>"
+  text3 += "<button id='delete-button' onclick='delist()'>삭제</button>"
   text3 += "</div>";
-
-
-
-  list.style.display="block";
+  list.style.display = "block";
 
   list.innerHTML = text3;
   unorderlist.appendChild(list);
 });
+
+const buttonclick2 = document.getElementsByClassName("delete-button");
+buttonclick2.addEventListener('click', () => {
+  confirm("삭제하시겠습니까?");
+  
+});
+
+const checkbox = document.querySelectorAll('input[type=checkbox]');
+
+function changeGreen() {
+  this.setAttribute("style", "background-color:green");
+}
+
+checkbox.addEventListener('click', changeGreen);
+
+
+
+function delist() {
+  const target = confirm("삭제");
+  checkboxOver.removeChild(target);
+};
